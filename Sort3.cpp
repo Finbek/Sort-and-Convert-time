44f0c8b033d12fcfc216db0a2041e38f5c606fb8
Dilshod Bakhtiyorov <46556037+Finbek@users.noreply.github.com>
2019-12-14 20:09:11 +0900

    Create Sort3.cpp

---
+++ Sort3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "Sort3.hpp"
+
+using namespace std;
+
+void Sort3(int& i, int& j, int& k)
+{if (i>j) {
+swap(i,j);}
+if(i>k){
+swap(i,k);}
+if(j>k){
+swap(j,k);}
+
+    // TODO: fill in the body.
+    // You MUST use the Swap function located in Sort3.hpp.
+}
+~         
